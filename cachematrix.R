9afa0ab9837214d98cf2a061dbdc60a74a6fb2e8
DavidBauer44 <david._____@___.___>
2020-01-29 13:59:16 -0500

    Initial commit of cachematrix.R file, answering Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2
+## Caching matrix inversion
+## https://github.com/davidbauer44/ProgrammingAssignment2
 
-## Write a short comment describing this function
 
+## Wrapper around a matrix that caches the inverse of the matrix
+## Uses get/set, per Java standard
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inverse) inv <<- inverse
+	getInverse <- function() inv
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Solve/invert a CacheMatrix object, using the existing cached copy if available
+## Ignoring all error handling
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+	inv <- x$getInverse()
+  if (is.null(inv)) {
+    inv <- solve(x$get(), ...)
+    x$setInverse(inv)
+  }
+	inv
 }
